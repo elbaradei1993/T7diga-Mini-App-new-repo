8c511c8c53fc333eff2470ba777d5c5cc420d023
elbaradei1993 <fatehielbaradei2030@gmail.com>
2025-03-21 19:17:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 8080;
 // Serve static files from the root directory
 app.use(express.static(path.join(__dirname)));
 
+// Route for the root URL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
